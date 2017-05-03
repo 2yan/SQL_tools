e7bd43bf56ff8214057ece1a857827547758cf6d
Ryan Francis <rfrancis@graphicproducts.local>
2017-05-03 09:45:44 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='ryan_sql',
-      version='0.23',
+      version='0.24',
       description='SQL Tools to make reading SQL a littler easier',
       url='https://github.com/2yan/ryan_sql',
       author='Ryan Francis',
       author_email='2yan@outlook.com',
       license='MIT',
       packages=['ryan_sql'],
-      install_requires= ['ryan_tools' ],
+      install_requires= ['pandas'],
       zip_safe=False)
